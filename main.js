00e023738a8287769f0e1b307c1907d05843dd33
Ignacy Łątka <74246391+LatekVo@users.noreply.github.com>
2023-09-08 17:51:31 +0200

    Update the project to the final version
    - refined UI
    - added analog clock

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let dateHandler = document.getElementById('date-container');
 let timeHandler = document.getElementById('time-container');
 let clockHandler = document.getElementById('clock-container');
+let clockHandlerHour = document.getElementById('clock-arm-hour');
+let clockHandlerMinute = document.getElementById('clock-arm-minute');
+let clockHandlerSecond = document.getElementById('clock-arm-second');
 
 let dateOptions = { 
     weekday: 'long', 
@@ -13,5 +16,13 @@ setInterval(() => {
     let dateObject = new Date();
     dateHandler.innerHTML = dateObject.toLocaleDateString('pl-PL', dateOptions);
     timeHandler.innerHTML = `${dateObject.toLocaleTimeString('pl-PL')}.${dateObject.getMilliseconds().toString()[0]}`;
-    console.log('update');
+
+    let secondDeg = dateObject.getSeconds();
+    let minuteDeg = dateObject.getMinutes() + (dateObject.getSeconds() / 60);
+    let hourDeg = dateObject.getHours() + (dateObject.getMinutes() / 60);
+
+    clockHandlerHour.style.rotate = `${hourDeg*30}deg`;
+    clockHandlerMinute.style.rotate = `${minuteDeg*6}deg`
+    clockHandlerSecond.style.rotate = `${secondDeg*6}deg`
+
 }, 100);
